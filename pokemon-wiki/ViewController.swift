1a7b404a3ec26c0a099e14922994a6569d0a9c0c
vonkad <2493345@gmail.coom>
2022-08-14 23:53:11 +0300

    load data and show in tableView, add searchConroller in navigationItem, filtered pokemons for search

--- pokemon-wiki/ViewController.swift
+++ pokemon-wiki/ViewController.swift
@@ -8,34 +8,112 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    private lazy var searchTextField: UISearchTextField = {
-        let searchTextField = UISearchTextField()
-        searchTextField.backgroundColor = .blue
-        return searchTextField
-    }()
+    
+    private let searchController = UISearchController(searchResultsController: nil)
     
     private lazy var tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.backgroundColor = .green
+        let tableView = UITableView(frame: view.frame)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         return tableView
+    }()
+    
+    private var model: [ResultsModel] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    private var filterModel: [ResultsModel] = []
+    private var isSearchEmpty: Bool {
+        guard let text = searchController.searchBar.text else { return false }
+        return text.isEmpty
+    }
+    private var isFiltering: Bool {
+        return searchController.isActive && !isSearchEmpty
+    }
+    
+    private lazy var activityView: UIActivityIndicatorView = {
+        let activityView = UIActivityIndicatorView(style: .large)
+        activityView.hidesWhenStopped = true
+        return activityView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        title = "Pokemons"
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        setLayout()
+        loadPokemons()
     }
 
+    private func loadPokemons() {
+        activityView.startAnimating()
+        Network.loadPokemons("https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0") { model in
+            self.model = model.results.sorted(by: {$0.name < $1.name})
+            self.activityView.stopAnimating()
+        }
+    }
+    
+    private func filterForSearch(_ searchText: String) {
+        filterModel = model.filter { $0.name.lowercased().contains(searchText.lowercased())}
+        tableView.reloadData()
+    }
+    
     private func setLayout() {
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        searchTextField.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(searchTextField)
+        activityView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
+        view.addSubview(activityView)
         NSLayoutConstraint.activate([
-            searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            
+            activityView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            activityView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
         ])
     }
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isFiltering ? filterModel.count : model.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let item = isFiltering ? filterModel[indexPath.row] : model[indexPath.row]
+        cell.textLabel?.text = item.name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let pokemon = isFiltering ? filterModel[indexPath.row] : model[indexPath.row]
+        let alert = UIAlertController(title: "This is \(pokemon.name)", message: "\n\n\n", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Clouse", style: .destructive, handler: nil))
+        
+        Network.loadImage(pokemon.url) { image in
+            let image = UIImageView(image: image)
+            alert.view.addSubview(image)
+            image.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                image.centerXAnchor.constraint(equalTo: alert.view.centerXAnchor),
+                image.centerYAnchor.constraint(equalTo: alert.view.centerYAnchor),
+                image.widthAnchor.constraint(equalToConstant: 64),
+                image.heightAnchor.constraint(equalToConstant: 64)])
+        }
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else { return }
+        filterForSearch(text)
+    }
+}
